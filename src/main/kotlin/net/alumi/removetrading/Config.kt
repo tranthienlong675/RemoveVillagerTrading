e3d3be71731abba98416cfdc526f70863fe6e5c6
tranthienlong675 <tranthienlong675@gmail.com>
2026-07-27 23:52:38 +0700

    Remove unnecessary config

--- src/main/kotlin/net/alumi/removetrading/Config.kt
+++ src/main/kotlin/net/alumi/removetrading/Config.kt
@@ -10,37 +10,6 @@ import java.util.function.Supplier
 // Demonstrates how to use Neo's config APIs
 object Config {
     private val BUILDER = ModConfigSpec.Builder()
-
-    @JvmField
-    val LOG_DIRT_BLOCK: ModConfigSpec.BooleanValue = BUILDER
-        .comment("Whether to log the dirt block on common setup")
-        .define("logDirtBlock", true)
-
-    @JvmField
-    val MAGIC_NUMBER: ModConfigSpec.IntValue = BUILDER
-        .comment("A magic number")
-        .defineInRange("magicNumber", 42, 0, Int.Companion.MAX_VALUE)
-
-    @JvmField
-    val MAGIC_NUMBER_INTRODUCTION: ModConfigSpec.ConfigValue<String?> = BUILDER
-        .comment("What you want the introduction message to be for the magic number")
-        .define<String?>("magicNumberIntroduction", "The magic number is... ")
-
-    // a list of strings that are treated as resource locations for items
-    @JvmField
-    val ITEM_STRINGS: ModConfigSpec.ConfigValue<List<String>> = BUILDER
-        .comment("A list of items to log on common setup.")
-        .defineListAllowEmpty(
-            "items",
-            listOf("minecraft:iron_ingot"),
-            { "" },
-            this::validateItemName
-        )
-
     @JvmField
     val SPEC: ModConfigSpec = BUILDER.build()
-
-    private fun validateItemName(obj: Any?): Boolean {
-        return obj is String && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(obj))
-    }
 }
